407519624fcf129e24437ea0cc6e3f8fc70a26fa
erfenjiao <630166475@qq.com>
2023-06-18 16:03:00 +0800

    TimerId.h

--- muduo/net/Callbacks.h
+++ muduo/net/Callbacks.h
@@ -89,6 +89,7 @@ namespace muduo
         class Buffer;
         class TcpConnection;
         typedef std::shared_ptr<TcpConnection> TcpConnectionPtr;
+        
         typedef std::function<void()> TimerCallback;
         typedef std::function<void (const TcpConnectionPtr&)> ConnectionCallback;
         typedef std::function<void (const TcpConnectionPtr&)> CloseCallback;
---
+++ muduo/net/TimerId.h
@@ -0,0 +1,44 @@
+
+#ifndef MUDUO_NET_TIMERID
+#define MUDUO_NET_TIMERID
+
+#include "../base/copyable.h"
+
+#include <cstdint>
+#include <cstddef>
+
+namespace muduo
+{
+    namespace net
+    {
+        class Timer;
+
+        class TimerId : public muduo::copyable {
+            public:
+
+            TimerId() : timer_(NULL),
+                        sequence_(0)
+            {
+            }
+
+            TimerId(Timer* timer, int64_t seq)
+                : timer_(timer),
+                sequence_(seq)
+            {
+            }
+
+            // default copy-ctor, dtor and assignment are okay
+
+            friend class TimerQueue;
+
+
+            private:
+            Timer* timer_;
+            int64_t sequence_;
+        };
+    } // namespace net
+    
+} // namespace muduo
+
+
+#endif
